0f43dd043c97d920b401fef62a20bff33afc6f30
Priyansh <priyanshg615@gmail.com>
2022-05-07 20:29:58 +0530

    Set Up

--- Beezhive/beezpay/src/Components/HomePage/WaitlistButton/waitlistbutton.js
+++ Beezhive/beezpay/src/Components/HomePage/WaitlistButton/waitlistbutton.js
@@ -6,7 +6,13 @@ const Waitlist=()=>{
     return(
         <div className="waitlist">
             <div className="placeholder">Join Waitlist</div>
-            <div className="arrow"></div>
+            <div className="arrow">
+               <span className="arrow_holder">
+               <ion-icon name="play-sharp"></ion-icon>
+                <ion-icon name="play-sharp"></ion-icon>
+                <ion-icon name="play-sharp"></ion-icon>
+               </span>
+            </div>
         </div>
     );
 }
