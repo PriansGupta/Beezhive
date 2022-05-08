79ed38d0887b79a22500611650469ea95c67d9d6
Priyansh <priyanshg615@gmail.com>
2022-05-08 20:58:30 +0530

    Credit card

--- Beezhive/beezpay/src/Components/HomePage/Credit/credit.js
+++ Beezhive/beezpay/src/Components/HomePage/Credit/credit.js
@@ -1,14 +1,17 @@
 import React from "react";
 import "./credit.css";
 import back1 from "../../../Assets/credit_back_1.png";
-import back2 from "../../../Assets/credit_back_2.png";
-import credit from "../../../Assets/Credit.png";
 
 const Credit = () => {
   return (
     <div className="credit_card_details">
       <div className="card">
         <img className="card_2" src={back1} alt="Credit card"></img>
+        <div className="data">
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
       </div>
       <div></div>
     </div>
