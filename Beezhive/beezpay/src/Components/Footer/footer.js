b1133c0fbbcfe69aa316c6ee23b95af13d478f15
Priyansh <priyanshg615@gmail.com>
2022-05-09 02:04:34 +0530

    Footer

--- Beezhive/beezpay/src/Components/Footer/footer.js
+++ Beezhive/beezpay/src/Components/Footer/footer.js
@@ -1,16 +1,20 @@
 import React from "react";
-import "./footer.css"
+import "./footer.css";
 
-const Footer=()=>{
-
-    return (
-        <div className="footer">
-            <div className="connect">Connect With Use
-            <div className="platforms"></div>
-            </div>
-            <div className="about"></div>
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="connect">
+        Connect With Us
+        <div className="platforms">
+          <ion-icon name="logo-twitter"></ion-icon>
+          <ion-icon name="logo-facebook"></ion-icon>
+          <ion-icon name="logo-instagram"></ion-icon>
+          <ion-icon name="logo-youtube"></ion-icon>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default Footer;
