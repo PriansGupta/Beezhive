eb5ab2155b5be121610b17355f38fd12df7e510f
Priyansh <priyanshg615@gmail.com>
2022-05-08 19:23:35 +0530

    Credit card

--- Beezhive/beezpay/src/Components/HomePage/Credit/credit.js
+++ Beezhive/beezpay/src/Components/HomePage/Credit/credit.js
@@ -3,3 +3,14 @@ import "./credit.css";
 import back1 from "../../../Assets/credit_back_1.png";
 import bacck2 from "../../../Assets/credit_back_2.png";
 import Credit from "../../../Assets/Credit.png";
+
+
+const Credit=()=>{
+
+    return(
+        <div></div>
+    );
+
+}
+
+export default Credit;
