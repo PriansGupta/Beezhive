75947eb9c2ae71bf8b6c301e7cbab41d37bb944e
Priyansh <priyanshg615@gmail.com>
2022-05-08 19:22:39 +0530

    Credit card

---
+++ Beezhive/beezpay/src/Components/HomePage/Credit/credit.js
@@ -0,0 +1,5 @@
+import React from "react";
+import "./credit.css";
+import back1 from "../../../Assets/credit_back_1.png";
+import bacck2 from "../../../Assets/credit_back_2.png";
+import Credit from "../../../Assets/Credit.png";
