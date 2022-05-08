57540fda0c97a1c6bca6cddfc9e3e36ae55e5ac0
Priyansh <priyanshg615@gmail.com>
2022-05-08 21:50:13 +0530

    Credit card

--- Beezhive/beezpay/src/Components/HomePage/JoinWaitlist/join.js
+++ Beezhive/beezpay/src/Components/HomePage/JoinWaitlist/join.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./join.css";
+import cardlogo from "../../../Assets/Card_logo.png"
 
 const Join=()=>{
 return(
     <div className="Join_waitlist">
-        <h1>Join The waitlist Now</h1>
+        <h1>Join waitlist Now</h1>
         <p>No Hidden Fees.No Joining Fees.No Annual Fees</p>
         <button>Join Now</button>
+        <img className="intro" src={cardlogo} alt="intro"></img>
     </div>
 );
 }
